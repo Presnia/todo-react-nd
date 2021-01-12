153ef7d6004e6c17f1213481b25b00b8b597256f
Andrei Dzebysh <presnia@yahoo.com>
2021-01-12 23:14:54 +0300

    Added todo counter and changed the output order

--- src/todo-page/TodoPage.js
+++ src/todo-page/TodoPage.js
@@ -4,19 +4,20 @@ import TodoList from "../todo-list/TodoList";
 import ListFilter from "../list-filter/ListFilter";
 
 function TodoPage({ allTodo }) {
+    const allTodoCount= allTodo.length;
     return (
         <div className="todo-list-container">
             <h2>ToDOs of mine</h2>
             <h2>
                 <span id="total-counter-text">Total Todo Count: </span>
-                <span id="total-counter-number">0</span>
+                <span id="total-counter-number">{ allTodoCount }</span>
             </h2>
 
             <ListFilter />
 
             <div className="todo-list">
                 <div className="empty-list">Nothing to do</div>
-                <TodoList allTodo={ allTodo }/>
+                <TodoList allTodo={ allTodo } />
             </div>
         </div>
     );
